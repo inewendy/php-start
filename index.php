2aa7db8d14d25fd11105f0fdcd58dba4912f7619
Ine Wendy <inewendy@Air-van-Ine.lan>
2022-06-20 11:37:56 +0200

    Scalar data types in PHP

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wendy in the building</title>
+</head>
+<body>
+    <p> This is a paragraph<hr></p>
+    <?php
+    echo "This is a paragraph";
+    echo 10;
+    ?>
+
+        <p> This is a paragraph</p>
+
+</body>
+</html>
